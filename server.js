0ba20d9dff602de8889eafdd78f15851810874e7
mjsherwood <mjsherwood@gmail.com>
2023-05-30 14:08:50 -0700

    Activity 1 Sessions and Messages

--- server.js
+++ server.js
@@ -12,6 +12,29 @@ const app = express()
 const baseController = require("./controllers/base-controller.js")
 const utilities = require('./utilities');
 const errorController = require('./controllers/errorController')
+const session = require("express-session")
+const pool = require('./database/')
+
+/* ************************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionID',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
