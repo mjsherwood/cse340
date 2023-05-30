91e63ab722e037ef7e4d46ac7fba6f092c9a3a07
mjsherwood <mjsherwood@gmail.com>
2023-05-30 13:21:20 -0700

    Fixed error handling on routes

--- routes/inventory-route.js
+++ routes/inventory-route.js
@@ -1,15 +1,12 @@
 // Needed Resources
 const express = require("express")
 const router = new express.Router()
+const Util = require('../utilities/index.js')
 const invController = require('../controllers/inv-controller')
 
 // Route to build inventory by classification view
-//router.get("/type/:classificationID", invController.buildByClassificationID);
-router.get("/type/:classificationID", (req, res, next) => {
-    console.log("Route classificationID:", req.params.classificationID);
-    invController.buildByClassificationID(req, res, next);
-});
-
-router.get('/detail/:id', invController.getVehicleById);
+router.get("/type/:classificationID", Util.handleErrors(invController.buildByClassificationID));
+// Route to build vehicle details page
+router.get('/detail/:id', Util.handleErrors(invController.getVehicleById));
 
 module.exports = router;
